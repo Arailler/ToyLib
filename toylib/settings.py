137af97a178b086e8b984ac6ed395d4ee42b2470
Kevin Maouchi <maouchi76@hotmail.fr>
2021-09-03 10:39:24 +0200

    End of development

--- toylib/settings.py
+++ toylib/settings.py
@@ -26,7 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*8d9uxxgaz*l4by+acd39s_ube(&!pxx)vy)zwe6d$ou!-byun'
+if os.environ.get('ENV') == 'PRODUCTION':
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+else :
+    SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('ENV') == 'PRODUCTION':
